0b0d4cd3aafcb319c175966df82d43a6f81e8bfc
42xxx <71282957+42xxx@users.noreply.github.com>
2020-10-08 16:57:00 -0400

    Add files via upload

--- HW 2.py
+++ HW 2.py
@@ -1,6 +1,7 @@
-# FE 595 HW 2
+# FE 595 HW 2 Minghao Kang
 from requests_html import HTMLSession
 import pandas as pd
+import sys
 
 
 def web_Scraper(url, times, file_Name):
@@ -17,12 +18,13 @@ def web_Scraper(url, times, file_Name):
                 mylist.append(mytext)
                 mydf = pd.DataFrame(mylist)
             mydf.columns = ['text']
-            Name = mydf[(mydf['text'].str.contains('Name'))]
+            Name = mydf[mydf['text'].str.contains('Name')]
             Purp = mydf[mydf['text'].str.contains('Purpose')]
             combin_df = pd.concat([Name.reset_index(drop=True), Purp.reset_index(drop=True)], axis=1)
             final_df = pd.concat([final_df, combin_df], ignore_index=True)
     except:
-        print('ERROR, CHECK YOUR URL')
+        sys.stderr.write('WARNING: CHECK YOUR URL')
+        exit(2)
     final_df.columns = ['Name', 'Purpose']
     print(final_df)
     final_df.to_csv('./' + file_Name, index=False)
@@ -30,4 +32,4 @@ def web_Scraper(url, times, file_Name):
 
 if __name__ == '__main__':
     url = 'http://3.95.249.159:8000/random_company'
-    web_Scraper(url, 50, 'companies data2.csv')
+    web_Scraper(url, 50, 'companies data.csv')
